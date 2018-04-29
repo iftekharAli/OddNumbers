30bafc7be98e10df19cadd74cd5edc8f60531b46
ALI ASM IFTEKHAR <ali.iftekhar343@gmail.com>
2018-04-29 22:50:37 +0600

    Add project files.

---
+++ OddNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            Console.Write(j);
+
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
